76ce9c028d5bd299a6c099a39aea1a78aa66c874
Adriano <adriano_alves_2008@hotmail.com>
2022-11-28 19:35:06 -0300

    css cursos

--- src/pages/Cursos/index.js
+++ src/pages/Cursos/index.js
@@ -10,7 +10,158 @@ function Cursos() {
         <button> Mobile</button>
         <button>Outros Cursos</button>
       </ul>
-      <C.Content></C.Content>
+      <C.Content>
+        <p>
+          HTML5 e CSS3 parte 1: A primeira página da Web -
+          <strong> 8 Horas </strong>
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 2: Posicionamento, listas e navegação -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 4: Avançando no CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML e CSS: praticando HTML/CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          Bootstrap: criação de uma single-page responsiva -
+          <strong> 12 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 1: A primeira página da Web -
+          <strong> 8 Horas </strong>
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 2: Posicionamento, listas e navegação -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 4: Avançando no CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML e CSS: praticando HTML/CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          Bootstrap: criação de uma single-page responsiva -
+          <strong> 12 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 1: A primeira página da Web -
+          <strong> 8 Horas </strong>
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 2: Posicionamento, listas e navegação -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 4: Avançando no CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML e CSS: praticando HTML/CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          Bootstrap: criação de uma single-page responsiva -
+          <strong> 12 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 1: A primeira página da Web -
+          <strong> 8 Horas </strong>
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 2: Posicionamento, listas e navegação -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 4: Avançando no CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML e CSS: praticando HTML/CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          Bootstrap: criação de uma single-page responsiva -
+          <strong> 12 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 1: A primeira página da Web -
+          <strong> 8 Horas </strong>
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 2: Posicionamento, listas e navegação -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 4: Avançando no CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML e CSS: praticando HTML/CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          Bootstrap: criação de uma single-page responsiva -
+          <strong> 12 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 1: A primeira página da Web -
+          <strong> 8 Horas </strong>
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 2: Posicionamento, listas e navegação -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas -
+          <strong> 8 Horas </strong> <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML5 e CSS3 parte 4: Avançando no CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          HTML e CSS: praticando HTML/CSS -<strong> 8 Horas </strong>{' '}
+          <span>(ALURA)</span>
+        </p>
+        <p>
+          Bootstrap: criação de uma single-page responsiva -
+          <strong> 12 Horas </strong> <span>(ALURA)</span>
+        </p>
+      </C.Content>
     </C.Cursos>
   )
 }
--- src/pages/Cursos/styles.js
+++ src/pages/Cursos/styles.js
@@ -33,8 +33,44 @@ export const Cursos = styled.div`
 `
 
 export const Content = styled.div`
-  background-color: #ccc;
+  background-color: rgba(100, 100, 100, 0.7);
+  border-radius: 8px;
   margin-top: 40px;
   width: 90%;
-  min-height: 70vh;
+  height: 70vh;
+  overflow: scroll;
+  ::-webkit-scrollbar-track {
+    display: none;
+  }
+  ::-webkit-scrollbar {
+    background: rgba(0, 0, 0, 0.6);
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #ff328e90;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-track-piece {
+    background: rgba(0, 0, 0, 0.6);
+  }
+  ::-webkit-scrollbar-corner {
+    background: rgba(0, 0, 0, 0.6);
+  }
+  padding: 20px;
+
+  p {
+    color: #fff;
+    font-size: 20px;
+    padding: 5px;
+    border-bottom: 2px solid rgba(100, 100, 100, 0.9);
+    margin-bottom: 10px;
+  }
+
+  strong {
+    color: rgba(0, 0, 0, 0.6);
+  }
+
+  span {
+    color: #ff328e;
+    font-weight: bold;
+  }
 `
