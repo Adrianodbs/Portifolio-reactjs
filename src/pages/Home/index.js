51de9360fc1ee2223b25e5fff1c4299a73e35a62
Adriano <adriano_alves_2008@hotmail.com>
2023-08-10 14:28:09 -0300

    atualização na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,14 @@
 import foto from '../../img/perfil2.jpg'
 import * as C from './styles'
 
-import { FaReact, FaHtml5, FaCss3Alt, FaSass } from 'react-icons/fa'
+import { FaReact, FaJava, FaSass } from 'react-icons/fa'
 import { IoLogoJavascript } from 'react-icons/io'
-import { SiTypescript, SiStyledcomponents, SiTailwindcss } from 'react-icons/si'
+import {
+  SiTypescript,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiSpring
+} from 'react-icons/si'
 
 function Home() {
   return (
@@ -11,12 +16,13 @@ function Home() {
       <div className="top">
         <img src={foto} alt="foto de perfil" />
         <div className="texto">
-          <h1>Desenvolvedor Front-End | React</h1>
+          <h1>Desenvolvedor Full Stack | React</h1>
           <p>
-            Oi, me chamo Adriano Alves, tenho 29 anos, estudo programação desde
-            Outubro de 2021 e atualmente estou cursando uma Pós Graduação em
-            desenvolvimento Full Stack. Venho buscando desenvolver as minhas
-            habilidades em JavaScript, TypeScript e React.
+            Tenoh 2 anos de experiência no desenvolvimento web e atualmente
+            estou cursando duas pós-graduações (Desenvolvimento Full Stack e
+            Projetos de Aplicativos Móveis Multiplataforma), além de aprimorar
+            meus conhecimentos em React, JavaScript, TypeScript, Java e Spring
+            Boot.
           </p>
         </div>
       </div>
@@ -29,6 +35,8 @@ function Home() {
           <SiStyledcomponents color="#D06EAA" />
           <SiTailwindcss color="#07B0CE" />
           <FaSass color="#C76494" />
+          <FaJava color="#F7BA00" />
+          <SiSpring color="#6CAE3E" />
         </div>
       </div>
     </C.HomePage>
