83088cd9b8175f143f45b417fea8a804dc3f0c71
Adriano <adriano_alves_2008@hotmail.com>
2023-11-09 11:50:43 -0300

    Feat: create Stack

--- src/components/Stack/index.jsx
+++ src/components/Stack/index.jsx
@@ -1,8 +1,26 @@
+import styled from 'styled-components'
+
 export default function Stack({ img, title }) {
   return (
-    <div>
+    <Container>
       <img src={img} alt={title} />
       <span>{title}</span>
-    </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  padding: 12px;
+
+  img {
+    width: 60px;
+  }
+  span {
+    color: var(--gray);
+  }
+`
