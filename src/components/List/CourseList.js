91815b9afb0616b95828fdd8ec681dd215be5026
Adriano <adriano_alves_2008@hotmail.com>
2023-11-09 16:07:54 -0300

    Feat: course list

---
+++ src/components/List/CourseList.js
@@ -0,0 +1,339 @@
+export const CHtmlCss = [
+  {
+    title: 'HTML5 e CSS3 parte 1: A primeira página da Web',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'HTML5 e CSS3 parte 2: Posicionamento, listas e navegação',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'HTML5 e CSS3 parte 4: Avançando no CSS',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'HTML E CSS: PRATICANDO HTML/CSS',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'BOOTSTRAP: CRIAÇÃO DE UMA SINGLE-PAGE RESPONSIVA',
+    time: 12,
+    school: '(Alura)'
+  },
+  {
+    title: 'BOOTSTRAP 4: CRIANDO UMA LANDING PAGE RESPONSIVA',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'CSS: DISPONDO ELEMENTOS COM FLEXBOX E GRID',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'FLEXBOX: POSICIONE ELEMENTOS NA TELA',
+    time: 9,
+    school: '(Alura)'
+  },
+  {
+    title: 'CSS GRID: SIMPLIFICANDO LAYOUTS',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'CSS: CONSTRUINDO LAYOUTS COM GRID',
+    time: 6,
+    school: '(Alura)'
+  },
+  {
+    title: 'LAYOUTS RESPONSIVOS: TRABALHANDO COM LAYOUTS MOBILE',
+    time: 7,
+    school: '(Alura)'
+  },
+  {
+    title: 'ARQUITETURA CSS: DESCOMPLICANDO OS PROBLEMAS',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'GUIA DE ESTILOS: MONTANDO PÁGINAS COM COMPONENTES',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'SASS: CSS SINTATICAMENTE ESPETACULAR',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'TAILWIND CSS: ESTILIZANDO A SUA PÁGINA COM CLASSES UTILITÁRIAS',
+    time: 8,
+    school: '(Alura)'
+  }
+]
+
+export const CJavascript = [
+  {
+    title:
+      'LÓGICA DE PROGRAMAÇÃO I: OS PRIMEIROS PROGRAMAS COM JAVASCRIPT E HTML',
+    time: 16,
+    school: '(Alura)'
+  },
+  {
+    title:
+      'LÓGICA DE PROGRAMAÇÃO II: PRATIQUE COM DESENHOS, ANIMAÇÕES E UM JOGO',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: PRIMEIROS PASSOS COM A LINGUAGEM',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: MANIPULANDO O DOM',
+    time: 6,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: PROGRAMANDO A ORIENTAÇÃO A OBJETOS',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT PARA WEB: CRIE PÁGINAS DINÂMICAS',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: PROGRAMANDO NA LINGUAGEM DA WEB',
+    time: 20,
+    school: '(Alura)'
+  },
+  {
+    title: 'FUNDAMENTOS DO JAVASCRIPT: TIPOS, VARIÁVEIS E FUNÇÕES',
+    time: 12,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT NA WEB: VALIDAÇÃO DE FORMULÁRIOS E HTML5',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT NA WEB: ARMAZENANDO DADOS NO NAVEGADOR',
+    time: 6,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: CONHECENDO O BROWSER E PADRÕES DE PROJETO',
+    time: 12,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: INTERFACES E HERANÇA EM ORIENTAÇÃO A OBJETOS',
+    time: 12,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: CONSUMINDO E TRATANDO DADOS DE UMA API',
+    time: 6,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: MÉTODOS DE ARRAY',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: CRIANDO REQUISIÇÕES',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT: VALIDAÇÕES E RECONHECIMENTO DE VOZ',
+    time: 6,
+    school: '(Alura)'
+  },
+  {
+    title: 'JAVASCRIPT do básico ao avançado (Com Node.js e projetos)',
+    time: 24,
+    school: '(Udemy)'
+  }
+]
+
+export const CReact = [
+  {
+    title: 'REACT: DESENVOLVENDO COM JAVASCRIPT',
+    time: 14,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT: COMO OS COMPONENTES FUNCIONAM',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT: ABSTRAINDO SEU CSS COM STYLED COMPONENTS',
+    time: 6,
+    school: '(Alura)'
+  },
+  {
+    title: 'FUNDAMENTOS DE REACT: ESCREVENDO COM TYPESCRIPT',
+    time: 12,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT: LIDANDO COM ARQUIVOS ESTÁTICOS',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT: CONHECENDO A BIBLIOTECA REACT ROUTER',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT ROUTER: NAVEGAÇÃO EM UMA SPA',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT: GERENCIAMENTO DE ESTADOS GLOBAIS COM CONTEXTAPI',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT: DESENVOLVENDO UMA BIBLIOTECA DE COMPONENTES',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT: GERENCIAMENTO DE ESTADOS GLOBAIS COM REDUX',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'NEXT.JS: EXPLORANDO O FRAMEWORK',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT JS DO ZERO AO AVANÇADO NA PRÁTICA',
+    time: 23,
+    school: '(Udemy)'
+  },
+  {
+    title: 'React do zero a maestria (com hooks, router, API, projetos)',
+    time: 30,
+    school: '(Udemy)'
+  },
+  {
+    title: 'Projeto completo - NodeJs, React, React Native, TypeScript',
+    time: 24,
+    school: '(Udemy)'
+  },
+  {
+    title: 'React e TypeScript: Desenvolvendo um dashboard',
+    time: 17,
+    school: '(Udemy)'
+  },
+  {
+    title: 'NextJS do zero ao avançado na pratica 2023',
+    time: 13,
+    school: '(Udemy)'
+  },
+  {
+    title: 'Curso de React.Js e Next.Js (nível intermediário e avançado)',
+    time: 93,
+    school: '(Udemy)'
+  }
+]
+
+export const CMobile = [
+  {
+    title: 'REACT NATIVE: CRIANDO UM APP',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT NATIVE: UTILIZANDO E CRIANDO HOOKS',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT NATIVE: CRIANDO MENU E NAVEGANDO ENTRE TELAS',
+    time: 10,
+    school: '(Alura)'
+  },
+  {
+    title: 'REACT NATIVE: UTILIZANDO WEB API',
+    time: 8,
+    school: '(Alura)'
+  },
+  {
+    title: 'Projeto completo - NodeJs, React, React Native, TypeScript',
+    time: 24,
+    school: '(Udemy)'
+  },
+  {
+    title: 'React Native: Desenvolva APPs Nativas para Android e IOS',
+    time: 45,
+    school: '(Udemy)'
+  }
+]
+
+export const CBackEnd = [
+  {
+    title: 'Lógica de programação com Java',
+    time: 30,
+    school: '(Descomplica (Pós-Graduação))'
+  },
+  {
+    title: 'Strategies BackEnd',
+    time: 30,
+    school: '(Descomplica (Pós-Graduação))'
+  },
+  {
+    title: 'Essencial DataBase',
+    time: 30,
+    school: '(Descomplica (Pós-Graduação))'
+  },
+  {
+    title: 'Intermediary BackEnd',
+    time: 30,
+    school: '(Descomplica (Pós-Graduação))'
+  }
+]
+
+export const COutrosCursos = [
+  {
+    title: 'GIT E GITHUB: CONTROLE E COMPARTILHE SEU CÓDIGO',
+    time: 6,
+    school: '(Alura)'
+  },
+  {
+    title: 'ACESSIBILIDADE WEB PARTE 1: TORNANDO SEU FRONT-END INCLUSIVO',
+    time: 6,
+    school: '(Alura)'
+  },
+  {
+    title: 'EXPRESSÕES REGULARES: CAPTURANDO TEXTOS DE FORMA MÁGICA',
+    time: 12,
+    school: '(Alura)'
+  },
+  {
+    title: 'SEO: PRINCÍPIOS DA OTIMIZAÇÃO DE SITES',
+    time: 8,
+    school: '(Alura)'
+  }
+]
